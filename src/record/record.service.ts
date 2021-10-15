e7b7f5a88399fa480f3813235fd96f83b3075114
kkoon9 <rndrnjs2003@gmail.com>
2021-10-15 21:16:31 +0900

    feat: lru-cahce로 교체 후 recordService에 코드 추가
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/record/record.service.ts
+++ src/record/record.service.ts
@@ -7,6 +7,18 @@ import { DayRecordRepository } from "../dayRecord/dayRecord.repository";
 import { RecordDto } from "./dto/record.request.dto";
 import { RecordResponseDto } from "./dto/record.response.dto";
 import { WeekRecordRepository } from "../weekRecord/weekRecord.repository";
+const LRU = require("lru-cache");
+const options = {
+  max: 500,
+  maxAge: 1000 * 60 * 60,
+  length(n, key) {
+    return 1;
+  },
+  dispose(key, n) {
+    /* 데이터가 삭제된 후 호출 */
+  },
+};
+const cache = new LRU(options);
 
 @Injectable()
 export class RecordService {
@@ -19,6 +31,7 @@ export class RecordService {
 
   async insertRecordByUser(recordDto: RecordDto): Promise<RecordResponseDto> {
     const userId = recordDto.userId;
+    cache.set("2021-03-01", "SUN:1:10");
     const recordDataList = recordDto.recordData;
     for (let i = 0; i < recordDataList.length; i++) {
       const dayRecord = new DayRecord();
@@ -32,6 +45,8 @@ export class RecordService {
       dayRecord.stroke = recordDataList[i].stroke;
       dayRecord.calorie = recordDataList[i].calorie;
       dayRecord.beatPerMinute = recordDataList[i].beatPerMinute;
+      const tt = cache.get("2021-03-01").split(":");
+      console.log(tt[0], tt[1], tt[2]);
       await this.DayRecordRepository.save(dayRecord);
     }
 
