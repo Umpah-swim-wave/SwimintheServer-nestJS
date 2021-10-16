f0448b7a82a3f84535a026dc77c084e5437a792a
kkoon9 <rndrnjs2003@gmail.com>
2021-10-16 11:48:10 +0900

    fix: 오타 수정
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { typeOrmConfig } from "./config/typeorm.config";
 import "dotenv/config";
 import { DayRecordModule } from "./dayRecord/dayRecord.module";
 import { WeekRecordModule } from "./weekRecord/weekRecord.module";
-import { RecordModule } from "./record/Record.module";
+import { RecordModule } from "./record/record.module";
 
 @Module({
   imports: [
