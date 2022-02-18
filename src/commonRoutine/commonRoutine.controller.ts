f1c0a7fed8e7b7246261756b3318ad2a57240bd4
Chedda98 <twa05157@gmail.com>
2022-02-19 02:15:26 +0900

    Feat: 유저 루틴 조회 API

--- src/commonRoutine/commonRoutine.controller.ts
+++ src/commonRoutine/commonRoutine.controller.ts
@@ -1,21 +1,25 @@
-import { Body, Controller, Get, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, UseGuards, ValidationPipe } from "@nestjs/common";
 import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { CommonRoutineListResponseDto } from "./dto/commonRoutine.response.dto";
 import { CommonRoutineService } from "./commonRoutine.service";
 import { CommonRoutineListDto } from "./dto/commonRoutine.data.dto";
+import { AuthGuard } from "@nestjs/passport";
+import { GetUser } from "src/auth/get-user.decorator";
+import { User } from "src/auth/auth.entity";
 
 @ApiTags('commonRoutine')
 @Controller('commonRoutine')
+@UseGuards(AuthGuard())
 export class CommonRoutineController {
   constructor(private commonRoutineService: CommonRoutineService) {}
 
   @Get('/list')
   @ApiOperation({
-    summary: '기본 루틴 조회 API',
-    description: '어푸에서 추천하는 기본 루틴 조회 API',
+    summary: '전체 루틴 조회 API',
+    description: '전체 루틴 조회 API',
   })
   @ApiOkResponse({
-    description: '기본 루틴 조회 성공입니다.',
+    description: '전체 루틴 조회 성공입니다.',
     type: CommonRoutineListResponseDto,
   })
   commonRoutineList(
@@ -26,16 +30,32 @@ export class CommonRoutineController {
 
   @Get("/detail")
   @ApiOperation({
-    summary: "기본 루틴 상세 조회 API",
-    description: "어푸에서 추천하는 기본 루틴의 상세 조회 API",
+    summary: "전체 루틴 상세 조회 API",
+    description: "전체 루틴의 상세 조회 API",
   })
   @ApiOkResponse({
-    description: "기본 루틴 상세 조회 성공했습니다.",
+    description: "전체 루틴 상세 조회 성공입니다.",
     type: CommonRoutineListResponseDto,
   })
   commonRoutineDetail(
     @Body(ValidationPipe) commonRoutineDetailDto: CommonRoutineListDto
   ): Promise<CommonRoutineListResponseDto> {
     return this.commonRoutineService.getRoutineDetail(commonRoutineDetailDto)
+  }
+
+  @Get("/userList")
+  @ApiOperation({
+    summary: "유저 루틴 조회 API",
+    description: "유저 루틴 조회 성공입니다.",
+  })
+  @ApiOkResponse({
+    description: "유저 루틴 조회 성공입니다.",
+    type: CommonRoutineListResponseDto,
+  })
+  userRoutineList(
+    @Body(ValidationPipe) userRoutineListDto: CommonRoutineListDto,
+    @GetUser() user : User
+  ): Promise<CommonRoutineListResponseDto> {
+    return this.commonRoutineService.getUserRoutine(userRoutineListDto, user);
   }
 }
--- src/commonRoutine/commonRoutine.service.ts
+++ src/commonRoutine/commonRoutine.service.ts
@@ -5,12 +5,16 @@ import { CommonRoutineListResponseDto } from './dto/commonRoutine.response.dto';
 import { CommonRoutineListDto } from './dto/commonRoutine.data.dto';
 import utilResponse from 'src/common/response/util.response';
 import messageResponse from '../common/response/message.response';
+import { AuthRepository } from 'src/auth/auth.repository';
+import { User } from 'src/auth/auth.entity';
 
 @Injectable()
 export class CommonRoutineService {
   constructor(
     @InjectRepository(CommonRoutineRepository)
     private readonly CommonRoutineRepository: CommonRoutineRepository,
+    @InjectRepository(AuthRepository)
+    private readonly AuthRepository: AuthRepository,
   ) {}
 
   async getAllRoutine(
@@ -30,5 +34,20 @@ export class CommonRoutineService {
   ): Promise<CommonRoutineListResponseDto> {
     const result = await this.CommonRoutineRepository.find();
     return utilResponse.success(messageResponse.GET_COMMON_ROUTINE_DETAIL_SUCCESS, result);
+  }
+
+  async getUserRoutine(
+    userRoutineDto: CommonRoutineListDto,
+    user: User
+  ): Promise<CommonRoutineListResponseDto> {
+    const userId = user.id;
+    const users = await this.AuthRepository.find({ relations: ['commonRoutine'], where: {id: userId} });
+    // users.forEach((user) => {
+    //   console.log(user.commonRoutine)
+    // });
+    return utilResponse.success(
+      messageResponse.GET_USER_ROUTINE_SUCCESS,
+      users,
+    );
   }
 }
